b74079dc4198852ba13ee669791014996685a751
Ayman Ahmed <absi.ayman@gmail.com>
2018-03-04 23:00:09 +0300

    Add process_request method

--- atm solution/atm.py
+++ atm solution/atm.py
@@ -13,26 +13,31 @@ class ATM:
 
         if request > self.balance:
             print "Can't give you all this money !!"
-        elif request > 0:
-            self.balance = self.balance - request
-            while request > 0:
-                if request >= 100:
-                    print "give 100"
-                    request -= 100
-                elif request >= 50:
-                    print "give 50"
-                    request -= 50
-                elif request >= 10:
-                    print "give 10"
-                    request -= 10
-                elif request >= 5:
-                    print "give 5"
-                    request -= 5
-                else:
-                    print "give " + str(request)
-                    request -= request
-        else:
+        elif request <= 0:
             print "More than zero plz!"
+        else:
+            self.process_request(request)
+
+
+
+    def process_request(self, request):
+        self.balance = self.balance - request
+        while request > 0:
+            if request >= 100:
+                print "give 100"
+                request -= 100
+            elif request >= 50:
+                print "give 50"
+                request -= 50
+            elif request >= 10:
+                print "give 10"
+                request -= 10
+            elif request >= 5:
+                print "give 5"
+                request -= 5
+            else:
+                print "give " + str(request)
+                request -= request
 
 
 balance1 = 500

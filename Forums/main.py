6aa5958506e656c36070a69dc8c5613fc6207503
Ayman Ahmed <absi.ayman@gmail.com>
2018-03-05 20:08:27 +0300

    Add __str_ to print object

--- Forums/main.py
+++ Forums/main.py
@@ -7,9 +7,9 @@ post1 = models.Post(member1, "Title 1", "Content 1 Content 1 Content 1")
 post2 = models.Post(member1, "Title 2", "Content 2 Content 2 Content 2")
 post3 = models.Post(member2, "Title 3", "Content 3 Content 3 Content 3")
 
-print member1.name
-print member2.name
+print member1
+print member2
 
-print post1.member.name + ": [" + post1.title + "] " + post1.content
-print post2.member.name + ": [" + post2.title + "] " + post2.content
-print post3.member.name + ": [" + post3.title + "] " + post3.content
+print post1
+print post2
+print post3
--- Forums/models.py
+++ Forums/models.py
@@ -3,9 +3,14 @@ class Member:
         self.name = name
         self.age = age
 
+    def __str__(self):
+        return "%s, %d years" % (self.name, self.age)
 
 class Post:
     def __init__(self, member, title, content):
         self. member = member
         self.title = title
         self.content = content
+
+    def __str__(self):
+        return "%s: [%s] -> [%s]" % (self.member.name, self.title, self.content)
